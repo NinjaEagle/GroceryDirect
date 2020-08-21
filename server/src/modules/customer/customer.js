d4cef8c7a569acbc097f34ca1fc56a02d0916a22
Kevin Wang <kcwang06@gmail.com>
2020-08-21 17:14:27 -0400

    add jwt to customer

--- server/src/modules/customer/customer.js
+++ server/src/modules/customer/customer.js
@@ -2,18 +2,18 @@ import Customer from './customer.model'
 import { buildCustomerInfo } from './buildCustomerInfo'
 import { AuthServices } from '../../services/Auth'
 
-export const customerAuth = async(req, res, next) => {
+export const customerAuth = async (req, res, next) => {
 	const token = AuthServices.getTokenFromHeaders(req)
-	if(!token){
-		req user = null;
-		return res.sendStatus(401);
-	}
-	const customerr = await Customer.findById(token.id)
-	if(!customer){
-		req.user = null;
+	if (!token) {
+		req.user = null
 		return res.sendStatus(401)
 	}
-	req.user = customer;
+	const customer = await Customer.findById(token.id)
+	if (!customer) {
+		req.user = null
+		return res.sendStatus(401)
+	}
+	req.user = customer
 	return next()
 }
 export const getOrCreateCustomer = async (info, providerName) => {
--- server/src/modules/customer/customer.routes.js
+++ server/src/modules/customer/customer.routes.js
@@ -1,7 +1,12 @@
 import { Router } from 'express'
 import { create } from './customer.controller'
+import { customerAuth } from './customer'
 
 const routes = Router()
 routes.post('/', create)
+routes.get('/hello', customerAuth, (req, res) => {
+	console.log('req.user', req.user)
+	res.send(`Hello ${req.user.firstName}`)
+})
 
 export default routes
