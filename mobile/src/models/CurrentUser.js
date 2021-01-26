47f2611a3b33197efa6618f01631d13a4b354d07
Kevin Wang <kcwang06@gmail.com>
2021-01-26 13:22:04 -0500

    added address form&settings screen

--- mobile/src/models/CurrentUser.js
+++ mobile/src/models/CurrentUser.js
@@ -1,8 +1,16 @@
-import { types } from 'mobx-state-tree'
+import { types } from "mobx-state-tree";
+import { UserAddressModel } from "./UserAddresses";
 
-export const CurrentUserModel = types.model( 'CurrentUserModel', {
-	_id: types.identifier,
-	firstName: types.string,
-	lastName: types.string,
-	avatarUrl: types.maybe(types.string),
-})
+export const CurrentUserModel = types
+	.model("CurrentUserModel", {
+		_id: types.identifier,
+		firstName: types.string,
+		lastName: types.string,
+		avatarUrl: types.maybe(types.string),
+		addresses: types.optional(types.array(UserAddressModel), []),
+	})
+	.views((self) => ({
+		get addressesIsEmpty() {
+			return self.addresses.length === 0;
+		},
+	}));
